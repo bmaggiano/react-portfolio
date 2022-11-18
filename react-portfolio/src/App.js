194bd420b2e2acbce787289394f16ca6754296c9
bmaggiano <brandon.maggiano@gmail.com>
2022-11-18 16:50:46 -0700

    implemented mdb ui kit and get footer working

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,13 @@
+import './footer.css'
 import './App.css';
 import NavContainer from './components/NavContainer';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <NavContainer />
+      <Footer />
     </div>
   );
 }
--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,5 +1,46 @@
-export default function Footer() {
+// Using a new npm package for this footer componenet called mdb-react-ui-kit
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBIcon,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+  
+  export default function Footer() {
     return (
-        <h2>Footer</h2>
-    )
-}
+        <div className='footer'>
+            <p> © 2022 Copyright: Brandon Maggiano</p>
+      <MDBFooter className='text-center text-white'>
+        <MDBContainer>
+          <section className='mb-4'>
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='facebook-f' />
+            </MDBBtn>
+  
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='twitter' />
+            </MDBBtn>
+  
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='google' />
+            </MDBBtn>
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='instagram' />
+            </MDBBtn>
+  
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='linkedin-in' />
+            </MDBBtn>
+  
+            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+              <MDBIcon fab icon='github' />
+            </MDBBtn>
+          </section>
+        </MDBContainer>
+  
+      </MDBFooter>
+      </div>
+    );
+  }
