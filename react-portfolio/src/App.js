1f4382ac4967ff0b7a709975e356c3a41f49f0b8
bmaggiano <brandon.maggiano@gmail.com>
2022-11-18 15:24:35 -0700

    set up nav functionality and boiler plate functions. Also installed bootstrap

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavContainer from './components/NavContainer';
+
+function App() {
+  return (
+    <div>
+      <NavContainer />
+    </div>
+  );
+}
+
+export default App;
