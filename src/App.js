9ba3f395d8c71185bf8566f2b1295b21f710ea62
bmaggiano <brandon.maggiano@gmail.com>
2022-11-19 15:50:34 -0700

    retry commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavContainer from './components/NavContainer';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <NavContainer />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
